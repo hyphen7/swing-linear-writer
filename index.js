2d205599872ed6375f76371de11fb945df120a9d
hyphen7 <akikawarenon@gmail.com>
2022-12-14 06:45:12 +0000

    Cronタスク実行機能追加

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ const Express = require('express');
 const app = Express();
 const PORT = process.env.PORT || 3000;
 
+app.use(Express.json());
+
 app.get('/', (req, res) => {
     res.send("Hello, world!");
 });
 
-app.get('/api/v1/task1', (req, res) => {
-    res.send("Run Task1");
+app.post('/api/v1/task1', (req, res) => {
+    const key = req.body['x-cron-key'];
+    if (key == process.env.EXEC_KEY) {
+        console.log("Exec Task1");
+        res.send("Exec Task1");
+    } else {
+        console.log("Invalid KEY");
+        res.send("Invalid KEY");
+    }
 });
 
 app.listen(PORT, ()=>{
