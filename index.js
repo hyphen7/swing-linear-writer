bc08733b3595f77b3746fe588c712ded03dcedaa
hyphen7 <akikawarenon@gmail.com>
2023-02-01 04:26:39 +0000

    /api/v1/task1の処理を見直し

--- index.js
+++ index.js
@@ -3,25 +3,30 @@ const app = Express();
 const PORT = process.env.PORT || 3000;
 const task1 = require('./task1.js');
 
+const asyncWrapper = fn => {
+    return (req, res, next) => {
+        return fn(req, res, next).catch(next);
+    }
+};
+
 app.use(Express.json());
 
 app.get('/', (req, res, next) => {
     res.send("Hello, world!");
 });
 
-app.post('/api/v1/task1', (req, res, next) => {
-    console.log("Start Task1");
-    task1().then(() => {
+app.post('/api/v1/task1', asyncWrapper(
+    async (req, res, next) => {
+        console.log("Start Task1");
+        await task1();
         console.log("End Task1");
         res.json({ message: "Exec Task1" });
-    }).catch(error => {
-        next(error);
-    });
-});
+    }
+));
 
 app.use((req, res, next) => {
     console.log("Not Found");
-    res.status(404).json({ message: "Not Found"});
+    res.status(404).json({ message: "Not Found" });
 })
 
 app.use((err, req, res, next) => {
